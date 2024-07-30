fcdd144b6a1d0299ce7aebbdb3fcbe7b2f5cb7d9
LucianFE <lucianemanuel.f@gmail.com>
2024-07-30 07:55:08 +0300

    'insert data in app.js'

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,26 @@
 "use strict";
-import data from "../../data.js";
-console.log(data);
+const data = [
+  {
+    category: "Reaction",
+    score: 80,
+    icon: "./assets/images/icon-reaction.svg",
+  },
+  {
+    category: "Memory",
+    score: 92,
+    icon: "./assets/images/icon-memory.svg",
+  },
+  {
+    category: "Verbal",
+    score: 61,
+    icon: "./assets/images/icon-verbal.svg",
+  },
+  {
+    category: "Visual",
+    score: 72,
+    icon: "./assets/images/icon-visual.svg",
+  },
+];
 
 const components = document.querySelector(".components");
 window.addEventListener("DOMContentLoaded", displayData);
